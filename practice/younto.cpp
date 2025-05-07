637796f517aba22ca6ddacb1abd7a1042be49b71
BramTechxxvi <bramtechxxvi@gmail.com>
2025-05-07 11:45:21 +0100

    new commit

---
+++ practice/younto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void lol(int firstNumber, int secondNumber) {
+    cout << firstNumber + secondNumber << endl;
+
+}
+int main(){
+    cout << "Welcome to Bram C++ Practice" << endl;
+    int bola = 10, ade = 35;
+     lol(bola, ade);
+}
+
+
